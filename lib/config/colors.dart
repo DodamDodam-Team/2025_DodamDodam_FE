2c296ddfbe40793326e13d67e7a753679832c6eb
장우준 <lambda@smail.kongju.ac.kr>
2025-09-28 21:19:14 +0900

    [Design] Color 상수 변경

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   // Primary Colors
-  static const Color primary = Color(0xffffffff);
+  static const Color primary = Color(0xffFFCC00);
   static const Color secondary = Color(0xffffffff);
   static const Color tertiary = Color(0xffffffff);
 
   // Neutral Colors
-  static const Color background = Color(0xffffffff);
+  static const Color background = Color(0xffEEEDE9);
   static const Color surface = Color(0xffffffff);
-  //static const Color surfaceVariant = Color(0xffffffff);
+  static const Color surfaceVariant = Color(0xffEEEDE9);
 
   // "On" Colors (콘텐츠 색상)
   static const Color onPrimary = Color(0xffffffff);
   static const Color onSecondary = Color(0xffffffff);
-  static const Color onBackground = Color(0xffffffff);
-  static const Color onSurface = Color(0xffffffff);
+  static const Color onBackground = Color(0xff6C6C6C);
+  static const Color onSurface = Color(0xff000000);
 
   // System Colors
   static const Color error = Color(0xffffffff);
