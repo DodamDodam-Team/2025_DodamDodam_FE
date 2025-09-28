1877a463ad998230f74488d9f2a41855a96f6086
장우준 <lambda@smail.kongju.ac.kr>
2025-09-29 02:18:16 +0900

    [Rename] color.dart를 constants.dart로 변경

--- lib/view/main_pages/custom_bottom_navigation_bar.dart
+++ lib/view/main_pages/custom_bottom_navigation_bar.dart
@@ -1,4 +1,4 @@
-import 'package:dodamdodam_fe/config/colors.dart';
+import 'package:dodamdodam_fe/config/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
