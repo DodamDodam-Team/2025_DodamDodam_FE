1400946999f581c86585942b85576cddccbf755b
장우준 <lambda@smail.kongju.ac.kr>
2025-09-29 23:12:41 +0900

    [Feat] CustomCard에 margin 매개변수 추가

--- lib/view/main_pages/home/card.dart
+++ lib/view/main_pages/home/card.dart
@@ -3,17 +3,25 @@ import 'package:flutter/widgets.dart';
 
 class CustomCard extends StatelessWidget {
   final Widget? child;
-  final double width;
-  final double height;
+  //final double width;
+  //final double height;
+  final BoxConstraints constraints;
   final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
   final BoxDecoration decoration;
 
   const CustomCard({
     super.key,
     this.child,
-    this.width = 361,
-    this.height = 356,
+    //this.width = 361,
+    //this.height = 356,
+    this.constraints = const BoxConstraints(
+      minWidth: 361,
+      maxWidth: 361,
+      minHeight: 356,
+    ),
     this.padding = const EdgeInsets.symmetric(vertical: 16, horizontal: 10),
+    this.margin = EdgeInsets.zero,
     this.decoration = const BoxDecoration(
       color: AppColors.surface,
       borderRadius: BorderRadius.all(Radius.circular(8)),
@@ -23,12 +31,9 @@ class CustomCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      constraints: BoxConstraints(
-        minWidth: width,
-        maxWidth: width,
-        minHeight: height,
-      ),
+      constraints: constraints,
       padding: padding,
+      margin: margin,
       decoration: decoration,
       alignment: Alignment.topCenter,
       child: child,
