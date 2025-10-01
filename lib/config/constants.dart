c3e03990592f7f025059d2d01c81c47ac3f8b989
장우준 <lambda@smail.kongju.ac.kr>
2025-10-01 22:19:10 +0900

    [Feat] Color Palette 및 Text Style 추가

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -4,23 +4,227 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 final backend_url = dotenv.get('BACKEND_URL');
 
 class AppColors {
-  // Primary Colors
+  // Main Colors
   static const Color primary = Color(0xffFFCC00);
   static const Color secondary = Color(0xffffffff);
   static const Color tertiary = Color(0xffffffff);
 
-  // Neutral Colors
+  // BackGround Colors
   static const Color background = Color(0xffEEEDE9);
-  static const Color surface = Color(0xffffffff);
+  static const Color onBoardingBackground = Color(0xffFFFFFF);
+  static const Color surface = Color(0xffFBFBFB);
+  static const Color onBoardingSurface = Color(0xffE7E7E7);
   static const Color surfaceVariant = Color(0xffEEEDE9);
 
-  // "On" Colors (콘텐츠 색상)
-  static const Color onPrimary = Color(0xffffffff);
-  static const Color onSecondary = Color(0xffffffff);
-  static const Color onBackground = Color(0xff6C6C6C);
-  static const Color onSurface = Color(0xff000000);
+  static const Color action = Color(0xffA9A28A);
 
-  // System Colors
-  static const Color error = Color(0xffffffff);
-  static const Color onError = Color(0xffffffff);
+  static const Color wrongAnswer = Color(0xffEFDAD8);
+  static const Color correctAnswer = Color(0xffC6DEC3);
+  static const Color correctAnswerBorder = Color(0xff8EC38E);
 }
+
+class AppTextStyle {
+  static final Header header = Header();
+  static final Body body = Body();
+  static final Hint hint = Hint();
+  static final OnPrimary onPrimary = OnPrimary();
+  static final OnAction onAction = OnAction();
+}
+
+class Header {
+  static const TextStyle large = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 20.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle medium = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 18.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle mediumBold = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 18.0,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle small = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 16.0,
+    fontWeight: FontWeight.w400,
+  );
+}
+
+class Body {
+  static const TextStyle large = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle largeBold = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle medium = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle mediumBoldUnderLine = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+    decoration: TextDecoration.underline,
+  );
+
+  static const TextStyle small = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle extraSmall = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 9.75,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle extraSmallGray = TextStyle(
+    color: Color(0xFF989898),
+    fontSize: 9.75,
+    fontWeight: FontWeight.w400,
+  );
+}
+
+class Hint {
+  static const TextStyle large = TextStyle(
+    color: Color(0xFF6C6C6C),
+    fontSize: 18.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle medium = TextStyle(
+    color: Color(0xFF6C6C6C),
+    fontSize: 16.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle small = TextStyle(
+    color: Color(0xFF6C6C6C),
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+  );
+}
+
+class OnPrimary {
+  static const TextStyle extraLarge = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 18.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle large = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle medium = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle small = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+  );
+}
+
+class OnAction {
+  static const TextStyle extraLarge = TextStyle(
+    color: Color(0xFFFFFFFF),
+    fontSize: 18.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle large = TextStyle(
+    color: Color(0xFFFFFFFF),
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle medium = TextStyle(
+    color: Color(0xFFFFFFFF),
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle small = TextStyle(
+    color: Color(0xFFFFFFFF),
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+  );
+}
+
+
+
+
+
+/**
+ * class header{}
+  static const TextStyle headerLarge = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 20.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle headerMedium = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 18.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle bodyMedium = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle bodyMediumSemiBold = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 16.0,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle bodySmall = TextStyle(
+    color: Color(0xFF000000),
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle hintLarge = TextStyle(
+    color: Color(0xFF6C6C6C),
+    fontSize: 18.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle hintMedium = TextStyle(
+    color: Color(0xFF6C6C6C),
+    fontSize: 14.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle hintSmall = TextStyle(
+    color: Color(0xFF6C6C6C),
+    fontSize: 12.0,
+    fontWeight: FontWeight.w400,
+  );
+ */
